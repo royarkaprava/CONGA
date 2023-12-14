eb6b80ac2c9226c36701e772e361fb2b68b4abff
royarkaprava <arkapravaroy0075@gmail.com>
2023-12-14 13:41:35 -0500

    Update pocal.cpp

--- Modified sampling steps/pocal.cpp
+++ Modified sampling steps/pocal.cpp
@@ -24,12 +24,23 @@ using namespace arma;
 // [[Rcpp::export]]
 int posel(int po, mat &X){
   vec tes = zeros(2*po);
-  
+  int inc = 0;
   for(int i = 0; i < (2*po); i++){
     tes(i) = accu(pow(cov(pow(atan(X),(i+1)/10))-cov(X),2)); //
+    if(i > 0){
+      if(tes(i) > tes(i-1)){
+        inc = inc + 1;
+      }
+      if(tes(i) < tes(i-1)){
+        inc = 0;
+      }
+    }
+    if(inc > 10){ //the distance is increasing for last 10 iterations so stop.
+      break;
+    }
   }
   
-  double ret = (index_min( tes )+1)/10;
+  double ret = (index_min( tes.elem( find(tes > 0) ) )+1)/10;
   
   return ret;
 }
